956c9d6c47dc9616a1c3e44f52b2569cb6f759be
Danny <dannybruzz@hotmail.com>
2022-10-14 18:31:07 +0800

    first draft

--- Leaflet-Part-1.js
+++ Leaflet-Part-1.js
@@ -1,22 +1,27 @@
 
 function createMap(data) {
-let streetmap =    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
+let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
 
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
 
 let baseMaps = {
-   "Street Map": streetmap
+   "Street Map": street,
+   "Topography":topo
   };
+   
 
 let overlayMaps = {
-    "Eathquakes": data
+    "Eathquakes": data,
 };
 
 let myMap = L.map("map", {
     center: [15.33, 32.93],
     zoom: 3,
-    layers: [streetmap, data]
+    layers: [street, data]
     });  
 L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
@@ -25,6 +30,7 @@ L.control.layers(baseMaps, overlayMaps, {
 }
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then(createMarkers);
+
 
 function createMarkers(response) {
 
@@ -35,18 +41,55 @@ function createMarkers(response) {
     let eqMarkers = [];
   
     // Loop through the stations array.
-    for (let i = 0; i < earthquakes.length; i++) {
-        let row = earthquakes[i]
+    for (let i = 0; i < earthquakes.length; i++) {  
+        let row = earthquakes[i];
         let details = row.properties;
         let geometry = row.geometry;
+        let Fcolor = "";
+        let depth = geometry.coordinates[3];
+        console.log(details.mag)
+        if (depth > 90) {
+          Fcolor = "black";
+        }
+        else if (depth > 70) {
+          Fcolor = "purple";
+        }
+        else if (depth > 50) {
+          Fcolor = "violet";
+        }
+        else if (depth > 30) {
+          Fcolor = "red";
+        }
+        else if (depth > 10) {
+            Fcolor = "orange";
+        }
+        else {
+            Fcolor = "yellow";
+        } 
+       
+        if (details.mag > 8) {
+            radiusValue = 500000;
+          }
+          else if (details.mag > 6) {
+            radiusValue = 400000;
+          }
+          else if (details.mag > 4) {
+            radiusValue = 300000;
+          }
+          else if (details.mag > 2) {
+            radiusValue = 200000;
+          }
+          else {
+            radiusValue = 100000;
+          } 
+        let earthquakesMarker = L.circle([geometry.coordinates[1], geometry.coordinates[0]], {
+            fillOpacity: 0.75,
+            color: "white",
+            fillColor: Fcolor,
+            radius: radiusValue
+        }).bindPopup("<h3>" + details.place + "<h3><h3>Magnitude: " + details.mag + "</h3>");
         
-        let earthquakesMarker = L.marker([geometry.coordinates[1], geometry.coordinates[0]])
-        .bindPopup("<h3>" + details.place + "<h3><h3>Magnitude: " + details.mag + "</h3>");
-  
-      // Add the marker to the bikeMarkers array.
       eqMarkers.push(earthquakesMarker);
     }
-  
-    // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
     createMap(L.layerGroup(eqMarkers));
   }
